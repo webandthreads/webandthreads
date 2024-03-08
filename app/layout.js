d043cbf5e0e636ad02f129b5ca4a15c81d9b493d
Tadious Bohwasi <tadious@Tadiouss-MacBook-Air.local>
2024-03-08 13:41:10 +0100

    first website deploy

--- app/layout.js
+++ app/layout.js
@@ -5,8 +5,8 @@ import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Web and Threads",
-  description: "A company that makes websites and threads",
+  title: "Web and stacks",
+  description: "Your partner in innovation.",
 };
 
 export default function RootLayout({ children }) {
@@ -15,8 +15,13 @@ export default function RootLayout({ children }) {
       <Head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-        <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest"></link>
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
